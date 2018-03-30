1db20f069ae491990fdf16ba703c6d86e1530c4a
hulibo <andy_hulibo@163.com>
2018-03-30 16:47:30 +0800

    bug修复

--- trade/src/com/bfly/trade/config/SpringConfig.java
+++ trade/src/com/bfly/trade/config/SpringConfig.java
@@ -1,19 +1,29 @@
 package com.bfly.trade.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
 
 import com.bfly.trade.interceptor.ApiInterceptor;
 import com.bfly.trade.interceptor.LoginInterceptor;
 
 /**
- * Spring配置
+ * SpringMVC配置
  * @author andy_hulibo@163.com
  * 2018年1月8日下午2:33:57
  */
 @Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.bfly.trade",useDefaultFilters=false,includeFilters=@Filter(type=FilterType.ANNOTATION,value=Controller.class))
 public class SpringConfig extends WebMvcConfigurerAdapter{
 
 	@Autowired
@@ -24,6 +34,17 @@ public class SpringConfig extends WebMvcConfigurerAdapter{
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(apiInterceptor).addPathPatterns("/api/**");
-		registry.addInterceptor(loginInterceptor).addPathPatterns("/**");
+		registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/api/**");
 	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp().prefix("/").suffix(".jsp").viewClass(JstlView.class);
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
 }
