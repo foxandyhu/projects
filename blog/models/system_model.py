8a12bf8174d7f55aab96edc7da691a32cf42fd82
andy_hulibo@163.com <andy_hulibo@163.com>
2018-07-11 22:03:34 +0800

    访问实体类

--- blog/models/system_model.py
+++ blog/models/system_model.py
@@ -50,3 +50,24 @@ class SysBanner(Serializable, db.Model):
     action = db.Column(db.String(10))  # 打开方式
     seq = db.Column(db.Integer, default=0)  # 排序序号
     logo = db.Column(db.String(200), nullable=False)  # 图片地址
+
+
+class SysAccess(Serializable, db.Model):
+    """系统访问统计"""
+
+    __tablename__ = "sys_access"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    access_time = db.Column(db.Time, nullable=False)  # 访问时间
+    access_date = db.Column(db.Date, nullable=False)  # 访问日期
+    access_ip = db.Column(db.String(50), nullable=False)  # 访问IP
+    access_area = db.Column(db.String(50))  # 访问区域
+    access_source = db.Column(db.String(250))  # 访问来源
+    external_link = db.Column(db.String(255))  # 外部连接地址
+    engine = db.Column(db.String(50))  # 访问搜索引擎
+    entry_page = db.Column(db.String(255))  # 入口页面
+    stop_page = db.Column(db.String(255))  # 最后停留页面
+    visit_second = db.Column(db.Integer)  # 访问时长/秒
+    visit_page_count = db.Column(db.Integer)  # 访问页面数
+    operating_system = db.Column(db.String(50))  # 操作系统
+    browser = db.Column(db.String(50))  # 浏览器
+    keyword = db.Column(db.String(255))  # 来访关键字
